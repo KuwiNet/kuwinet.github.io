0bff23c441a4601d93c4a9736d23ee34476a8266
KuWi <darnycn@gmail.com>
2025-04-18 01:30:44 +0800

    更新 jnav.js

--- js/jnav.js
+++ js/jnav.js
@@ -1,6 +1,6 @@
 // 使用 fetch 方法加载导航栏
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('navbar.html')
+    fetch('https://kuwinet.github.io/navbar.html')
 .then(response => response.text())
 .then(data => {
     document.getElementById('navbar-container').innerHTML = data;
