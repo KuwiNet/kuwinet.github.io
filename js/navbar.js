7d2b4686d8c084bdc4759251755857606c32d535
KuWi <darnycn@gmail.com>
2025-04-18 01:55:01 +0800

    更新 navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -47,13 +47,31 @@ $(".navbar-toggler").click(function () {
 });
 
 // --------------add active class-on another-page move----------
-jQuery(document).ready(function ($) {
-    var path = window.location.pathname.split("/").pop();
-    
-    if (path == '') {
-path = 'index.html';
-    }
-    
-    var target = $('#navbarSupportedContent ul li a[href="' + path + '"]');
-    target.parent().addClass('active');
+// 使用 JavaScript 为当前页面的导航项添加 active 类
+document.addEventListener('DOMContentLoaded', function() {
+    // 获取当前页面的完整 URL
+    var currentURL = window.location.href;
+    // 去除协议头和斜杠
+    currentURL = currentURL.replace(/^(https?:\/\/)?(www\.)?/, '').split('/')[0];
+
+    // 遍历导航菜单中的所有链接
+    $('#navbarSupportedContent ul li a').each(function () {
+var $this = $(this);
+// 获取链接的 href 属性值
+var linkURL = $this.attr('href');
+// 去除协议头和斜杠
+linkURL = linkURL.replace(/^(https?:\/\/)?(www\.)?/, '').split('/')[0];
+
+// 如果当前页面 URL 和链接 URL 一致
+if (currentURL === linkURL) {
+    // 为当前链接的父元素添加 active 类
+    $this.parent().addClass('active');
+    // 如果有子菜单，添加相应的样式类
+    $this.parents('.menu-submenu').addClass('show-dropdown');
+    $this.parents('.menu-submenu').parent().addClass('active');
+} else {
+    // 如果不一致，移除 active 类
+    $this.parent().removeClass('active');
+}
+    });
 });
