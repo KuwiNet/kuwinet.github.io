ff69c5eea45dbbd005bf1d0f70b862002f0af45d
KuWi <darnycn@gmail.com>
2025-04-19 23:25:00 +0800

    更新 navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -47,13 +47,30 @@ $(".navbar-toggler").click(function () {
 });
 
 // --------------add active class-on another-page move----------
-jQuery(document).ready(function ($) {
-    var path = window.location.pathname.split("/").pop();
-    
-    if (path == '') {
-path = 'index.html';
-    }
-    
-    var target = $('#navbarSupportedContent ul li a[href="' + path + '"]');
-    target.parent().addClass('active');
+//jQuery(document).ready(function ($) {
+//    var path = window.location.pathname.split("/").pop();
+//    
+//    if (path == '') {
+//path = 'index.html';
+//    }
+//    
+//    var target = $('#navbarSupportedContent ul li a[href="' + path + '"]');
+//    target.parent().addClass('active');
+//});
+// 使用 JavaScript 为当前页面的导航项添加 active 类
+document.addEventListener('DOMContentLoaded', function() {
+    var currentURL = window.location.href;
+    currentURL = currentURL.replace(/^(https?:\/\/)?(www\.)?/, '').split('/')[0];
+    $('#navbarSupportedContent ul li a').each(function () {
+var $this = $(this);
+var linkURL = $this.attr('href');
+linkURL = linkURL.replace(/^(https?:\/\/)?(www\.)?/, '').split('/')[0];
+if (currentURL === linkURL) {
+    $this.parent().addClass('active');
+    $this.parents('.menu-submenu').addClass('show-dropdown');
+    $this.parents('.menu-submenu').parent().addClass('active');
+} else {
+    $this.parent().removeClass('active');
+}
+    });
 });
